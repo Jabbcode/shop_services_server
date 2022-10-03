12f0062d492cabbb2456888c3d3385ce37b4d558
Jabbcode <Jabbsontouch@gmail.com>
2022-10-03 11:12:20 -0400

    adding database connection in the application configuration and adding initial settings as environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 
+import { dbConenction } from './config/db'
+import dotenv from 'dotenv'
+
+dotenv.config() // Configuracion de variables de entorno
+
+dbConenction() // Conexion a la base de datos de MongoDB
+
+const PORT = process.env.PORT
+
 const app = express()
 
-app.use('/', (req, res) => {
-	res.send('Hello World')
-})
+app.use(express.json())
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000')
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
